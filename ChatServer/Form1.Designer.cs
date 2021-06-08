68d36a9386bda5a187f93b9291b9edc114779b09
BulbasDanil <bulbas_danil@protonmail.com>
2021-06-08 19:16:25 +0300

    add: Part of Server of SimpleChat

---
+++ ChatServer/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace ChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipField = new System.Windows.Forms.TextBox();
+            this.portField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statucLabel = new System.Windows.Forms.Label();
+            this.chatField = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.statucLabel);
+            this.groupBox1.Controls.Add(this.portField);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ipField);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(22, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Size = new System.Drawing.Size(359, 212);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.stopButton);
+            this.groupBox2.Controls.Add(this.startButton);
+            this.groupBox2.Location = new System.Drawing.Point(393, 22);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox2.Size = new System.Drawing.Size(242, 212);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Управление";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chatField);
+            this.groupBox3.Location = new System.Drawing.Point(22, 246);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox3.Size = new System.Drawing.Size(613, 317);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Журнал чата";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // ipField
+            // 
+            this.ipField.Location = new System.Drawing.Point(63, 41);
+            this.ipField.Name = "ipField";
+            this.ipField.Size = new System.Drawing.Size(287, 29);
+            this.ipField.TabIndex = 0;
+            this.ipField.Text = "127.0.0.1";
+            this.ipField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // portField
+            // 
+            this.portField.Location = new System.Drawing.Point(63, 92);
+            this.portField.Name = "portField";
+            this.portField.Size = new System.Drawing.Size(287, 29);
+            this.portField.TabIndex = 1;
+            this.portField.Text = "10101";
+            this.portField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // statucLabel
+            // 
+            this.statucLabel.AutoSize = true;
+            this.statucLabel.Location = new System.Drawing.Point(43, 152);
+            this.statucLabel.Name = "statucLabel";
+            this.statucLabel.Size = new System.Drawing.Size(287, 24);
+            this.statucLabel.TabIndex = 3;
+            this.statucLabel.Text = "Состояние сервера: выключен";
+            // 
+            // chatField
+            // 
+            this.chatField.Location = new System.Drawing.Point(15, 31);
+            this.chatField.Multiline = true;
+            this.chatField.Name = "chatField";
+            this.chatField.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatField.Size = new System.Drawing.Size(589, 277);
+            this.chatField.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(9, 41);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(224, 58);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Включить сервер";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(9, 118);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(224, 58);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Выключить сервера";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 578);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox ipField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label statucLabel;
+        private System.Windows.Forms.TextBox portField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox chatField;
+    }
+}
+
---
+++ ChatServer/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    public partial class Form1 : Form
+    {
+        int port;
+        IPAddress addr;
+        IPEndPoint ep;
+
+        Socket listener;
+        Thread workThread;
+        ManualResetEvent serverEvent;
+
+        byte[] buff1 = new byte[2048];
+        byte[] buff2 = new byte[2048];
+
+        string clientMessage = String.Empty;
+        string serverMessage = String.Empty;
+
+        int receiveBytes = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serverEvent = new ManualResetEvent(false);
+
+                addr = IPAddress.Parse(ipField.Text);
+                port = int.Parse(portField.Text);
+                ep = new IPEndPoint(addr, port);
+
+                listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                listener.Bind(ep);
+                listener.Listen(100);
+
+                workThread = new Thread(new ThreadStart(ThreadMethod));
+                workThread.IsBackground = true;
+                workThread.Start();
+
+                startButton.Enabled = false;
+                statucLabel.ForeColor = Color.Green;
+                statucLabel.Text = "Состояние сервера: включен";
+                chatField.Text += DateTime.Now.ToString() + " => Сервер успешно запущен \r\n";
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Ошибка потока", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        // 1
+        void ThreadMethod()
+        {
+            try
+            {
+                while (true)
+                {
+                    AsyncCallback delegate1 = new AsyncCallback(AcceptCallback);
+                    listener.BeginAccept(delegate1, listener);
+                    serverEvent.WaitOne();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Ошибка потока", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // 2
+        public void AcceptCallback(IAsyncResult state)
+        {
+
+        }
+    }
+}
